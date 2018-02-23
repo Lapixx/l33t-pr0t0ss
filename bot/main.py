7b3c16a437e23cbba083480c64bbeda98741072e
Pete Ropponen <b0untyh@reaktor.fi>
2018-02-23 12:21:24 +0200

    add cannon building

--- bot/main.py
+++ bot/main.py
@@ -12,7 +12,7 @@ class MyBot(sc2.BotAI):
 
     async def on_step(self, iteration):
         if iteration == 0:
-            await self.chat_send(f"Name: {self.NAME}")
+            await self.chat_send(f"I'd call you a tool, but that would imply you were useful in at least one way.")
 
             # available_workers = self.workers
             #
@@ -28,12 +28,20 @@ class MyBot(sc2.BotAI):
         await self.build_supply()
         await self.build_workers()
         await self.expand()
+        await self.build_strategy()
 
     async def build_workers(self):
         for cc in self.units(UnitTypeId.NEXUS).ready.noqueue:
             if cc.assigned_harvesters < cc.ideal_harvesters:
                 if self.can_afford(UnitTypeId.PROBE):
                     await self.do(cc.train(UnitTypeId.PROBE))
+
+    async def build_strategy(self):
+        if not self.has_building(UnitTypeId.FORGE):
+            await self.build_structure(self.units(UnitTypeId.NEXUS)[0], UnitTypeId.FORGE)
+
+        if self.has_building(UnitTypeId.FORGE):
+            await self.build_structure(self.units(UnitTypeId.NEXUS)[0], UnitTypeId.PHOTONCANNON)
 
     @sc2.BotAI.expansion_locations.getter
     def expansion_locations(self):
@@ -81,3 +89,12 @@ class MyBot(sc2.BotAI):
             if self.supply_left < 4 and not self.already_pending(UnitTypeId.PYLON):
                 if self.can_afford(UnitTypeId.PYLON):
                     await self.build(UnitTypeId.PYLON, near=cc.position.towards(self.game_info.map_center, 5))
+
+    async def build_structure(self, near, building):
+        if self.units(UnitTypeId.PYLON).ready.exists:
+            pylon = self.units(UnitTypeId.PYLON).closest_to(near)
+            if self.can_afford(building):
+                await self.build(building, near=pylon)
+
+    def has_building(self, unit_type):
+        return self.already_pending(unit_type) or self.units(unit_type).ready.exists
